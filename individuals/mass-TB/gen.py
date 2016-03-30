b30cb27a29da5eab4388c753a76350761ee67ac0
Takayuki Muranushi <muranushi@gmail.com>
2016-03-30 11:23:41 +0900

    c

--- individuals/mass-TB/gen.py
+++ individuals/mass-TB/gen.py
@@ -8,10 +8,16 @@ def gen(xsize, zsize, cascade,opts,tb):
     if cascade == 0:
         xs = [xsize/2+tb]
         zs = [zsize/2+tb]
+        if xsize/2 <= tb:
+            return
     elif cascade == 1:
         xs = [xsize/2+tb]
         zs = [zsize/3+tb, (2*zsize)/3+tb]
-    dirn = "b" +str(ctr)
+        if xsize <= 2*tb:
+            return
+        if xsize/3 <= tb:
+            return
+    dirn = "c" +str(ctr)
     ctr+=1
     subprocess.call("mkdir " + dirn, shell = True)
     fn = dirn + "/a.idv"
@@ -19,7 +25,7 @@ def gen(xsize, zsize, cascade,opts,tb):
         con = """
 compiler_flags: ['-O3', '-Kfast,parallel', '-Kocl', '-Klib', '-Koptmsg=2', '-Karray_private', '-Kinstance=8', '-Kdynamic_iteration', '-Kloop_fission', '-Kloop_part_parallel', '-Kloop_part_simd', '-Keval', '-Kreduction','-Kopenmp', '-Ksimd=2']
 cpp_sourcecode_url: /home/nushio/hub/formura/examples/pearson-3d-main.cpp
-fmr_sourcecode_url: /home/nushio/hub/formura/examples/pearson-3d.fmr
+fmr_sourcecode_url: /home/nushio/hub/formura/examples/pearson-3d-0M.fmr
 formura_version: 2f72606ab12501f67d8069d325c1304c60039d87
 numerical_config:
   initial_walls:
@@ -35,19 +41,19 @@ numerical_config:
         fp.write(con)
 
 optss = []
-# optss.append(['stick-single-comp', 'omp','omp-collapse', 'no-subroutine'])
-# optss.append(['stick-single-comp', 'omp','omp-collapse'])
-# optss.append(['stick-single-comp', 'no-subroutine'])
-# optss.append(['stick-single-comp'])
+optss.append(['stick-single-comp', 'omp','omp-collapse', 'no-subroutine'])
+optss.append(['stick-single-comp', 'omp','omp-collapse'])
+optss.append(['stick-single-comp', 'no-subroutine'])
+optss.append(['stick-single-comp'])
 optss.append(['stick-all-comp', 'omp','omp-collapse'])
 optss.append(['stick-all-comp', 'omp','omp-collapse', 'no-subroutine'])
 optss.append(['stick-all-comp'])
 
-for xsize in [64,128,256]:
-    for zsize in [64,128,256]:
+for xsize in [64,128,256,512]:
+    for zsize in [128,256,512,1024]:
         if xsize > zsize:
             continue
         for cascade in [0,1]:
             for opts in optss:
-                for tb in [1,2,4,8]:
+                for tb in [8,16,32]:
                     gen(xsize,zsize,cascade,opts,tb)
